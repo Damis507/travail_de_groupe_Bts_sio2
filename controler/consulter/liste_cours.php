4661401fb4c6b80d8473cf9d22d0f1667dbb1f24
Damis507 <118991947+Damis507@users.noreply.github.com>
2023-07-27 17:06:47 +0200

    Update liste_cours.php
    final version

--- controler/consulter/liste_cours.php
+++ controler/consulter/liste_cours.php
@@ -15,41 +15,45 @@
 				echo '<meta http-equiv="refresh" content="0;URL=liste_cours.php">';
 			}
 		}
-		else if (($_GET["info"] == "modifier_cours"))
+	/*	else if (($_GET["info"] == "assigner_eleve"))
 		{
-			if (isset($_GET['libelle_cours']) && $_GET['id_cours'] != '') 
+			if (isset($_GET['libelle_cours']) && $_GET['libelle_cours'] != '') 
 			{
-				$e = $managergestionnaire->get($_GET['libelle_cours']);
+				$cours = $managergestionnaire->selectAll();
 				
-				$_SESSION['item'] = array('id_cours' => $e['libelle_cours'],
-	                                       'heure_cours' => $e['nom_personnel'],
-										   'jour_cours' => $e['prenom_personnel'],
-										   'salle_cours' => $e['adresse_personnel'],
-										   'mail_personnel' => $e['mail_personnel']);
+				$_SESSION['cours'] = array();
 				
-				echo '<meta http-equiv="refresh" content="0;URL=../systeme/ajouter_personnel.php?option=modif.">';
+				foreach ($cours as $e)
+				{		
+					$_SESSION['cours'][] = array('libelle_cours' => $e['libelle_cours'],
+													'heure_cours' => $e['heure_cours'],
+													'jour_cours' => $e['jour_cours'],
+													'libelle_salle' => $e['libelle_salle']);
+				}
+				
+				echo '<meta http-equiv="refresh" content="0;URL=../systeme/liste_eleve.php?option=modif.">';
+
 			}
-		}
+		}*/
 	}
 	else
 	{
-		$personnel = $managerpersonnel->selectAll();
+		$cours = $managergestionnaire->selectAll();
 		
-		$_SESSION['personnel'] = array();
+		$_SESSION['cours'] = array();
 		
-		foreach ($personnel as $e)
+		foreach ($cours as $e)
 		{		
-			$_SESSION['personnel'][] = array('pseudo_personnel' => $e['pseudo_personnel'],
-                                                'nom_personnel' => $e['nom_personnel'],
-                                                'prenom_personnel' => $e['prenom_personnel'],
-                                                'adresse_personnel' => $e['adresse_personnel'],
-                                                'mail_personnel' => $e['mail_personnel']);
+			$_SESSION['cours'][] = array('libelle_cours' => $e['libelle_cours'],
+											'heure_cours' => $e['heure_cours'],
+											'jour_cours' => $e['jour_cours'],
+											'libelle_salle' => $e['libelle_salle']);
 		}
 		
 		/*echo "<pre>";
 		 print_r($_SESSION['employes']);
 		echo "</pre>";*/
 		
-		echo '<meta http-equiv="refresh" content="0;URL=../../?rubrique=consulter&operation=profil_personnel">';
+		echo '<meta http-equiv="refresh" content="0;URL=../../?rubrique=consulter&operation=liste_cours">';
 	}
 ?>
